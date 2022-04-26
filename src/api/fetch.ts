4c2dd0119ae0175dc068e8346ff6b277674852c9
fastAxiu <scut_sd@163.com>
2022-04-26 22:18:58 +0800

    feat: add fetch

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,5 +1,38 @@
-function proxyFetch() {
+import { CONFIG } from "../config";
+import { LOG } from "../util/log";
 
+const originalFetch = window.fetch;
+
+function proxyFetch() {
+    window.fetch = (url, config) => {
+        const baseData = {
+            startTime: Date.now(),
+            url,
+            method: config?.method,
+            type: 'fetch',
+        }
+
+        return originalFetch(url, config)
+            .then(res => {
+                const { status, ok: success } = res.clone();
+                LOG.report(CONFIG.reportUrl, {
+                    ...baseData,
+                    duration: Date.now() - baseData.startTime,
+                    status,
+                    success
+                })
+                return res;
+            })
+            .catch(err => {
+                LOG.report(CONFIG.reportUrl, {
+                    ...baseData,
+                    duration: Date.now() - baseData.startTime,
+                    status: -1,
+                    success: false
+                })
+                throw err;
+            })
+    }
 }
 
 export const FETCH = {
